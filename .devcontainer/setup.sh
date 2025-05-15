a53b82272fddb08b430d565cabb2f17a035f63ea
John Glossner <jglossner@rivier.edu>
2025-05-15 12:45:21 -0400

    Revise containers for uv

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,6 +2,8 @@
 
 echo "Creating uv environment."
 
+# install uv and venv
+curl -LsSf https://astral.sh/uv/install.sh | sh
 uv venv --python 3.12
 source .venv/bin/activate
 uv pip install -r requirements.txt
