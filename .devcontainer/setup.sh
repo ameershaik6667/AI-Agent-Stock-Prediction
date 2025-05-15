b147f7dc3ed5cf3c08a84d427ee6977134db7ae7
John Glossner <jglossner@rivier.edu>
2025-05-15 12:59:24 -0400

    Source uv environment during setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,6 +4,7 @@ echo "Creating uv environment."
 
 # install uv and venv
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 uv venv --python 3.12
 source .venv/bin/activate
 uv pip install -r requirements.txt
